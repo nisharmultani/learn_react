aab962330ce70ef24cb16a7f7aa68fb0d126b716
NisharMultani <nisharmultani7@gmail.com>
2023-09-25 16:22:30 +0530

    fix: -this is frist commir - this is second commit - tis is last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { Profile } from "./components/profile";
+
+const fullName = {
+  name: "nsihar",
+  lastname: "multani",
+};
+const kvArray = [
+  { key: 1, value: 10 },
+  { key: 2, value: 20 },
+  { key: 3, value: 30 },
+];
+
+const s =kvArray.map(({ key, value, name }) => {
+  return (
+    <div>
+      <h3>{value}</h3>
+      <h3>{key}</h3>
+    </div>
+  );
+})
+console.log(s)
 function App() {
-  return <div>nishar</div>;
+  return (
+    <>
+      {fullName.name}
+      <Profile name="nishar" lastname="ok" />
+      <Profile lila="lila" name="amishas" />
+      <Profile />
+      <Profile></Profile>
+      {  kvArray.map(({ key, value, name }) => {
+        return (
+          <div>
+            <h3>{value}</h3>
+            <h3>{key}</h3>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
